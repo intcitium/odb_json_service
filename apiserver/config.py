b220e106a1e086d512bc8d5181f9650d65841746
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2019-06-25 11:24:54 +0200

    Create config.py

---
+++ apiserver/config.py
@@ -0,0 +1,8 @@
+HOST_IP = "YOUR SERVER IP THAT IS EXPOSED TO THE INTERNET"
+HTTPS = "https://%s" % HOST_IP
+SECRET_KEY = 'AweseomelySecretKeyThatNobodyCanGuessJustTryandCrack!tComeon'
+MAIL_USERNAME = 'YOUR GMAIL'
+MAIL_PASSWORD = 'YOUR GMAIL PASSWORD'
+COPILOT_URL = 'https://api.cai.tools.sap/auth/v1/owners/YOUR ACCOUNT'
+COPILOT_AUTH = 'YOUR TOKEN'
+COPILOT_DEV_TOKEN = 'Token YOUR TOKEN'
