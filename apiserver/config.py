af8bd9510c5fc52f9c3b5f4525b3b07dcfedb63e
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2019-08-07 09:08:23 +0200

    Delete config.py

--- apiserver/config.py
+++
@@ -1,8 +0,0 @@
-HOST_IP = "localhost"
-HTTPS = "https://%s" % HOST_IP
-SECRET_KEY = '7RmkKssd$gFLRM8y)^RbDjj,Sj)YCIFbadT!dOD&#,B&QAqIfE'
-MAIL_USERNAME = 'opennetgraphservice@gmail.com'
-MAIL_PASSWORD = 'jVc85pw!%S#xz'
-COPILOT_URL = 'https://api.cai.tools.sap/auth/v1/owners/lscheinman'
-COPILOT_AUTH = '09ba69772f1a9ca9552227a90d92e076'
-COPILOT_DEV_TOKEN = 'Token e4ed32c26eae10309bd335e30190621e'
