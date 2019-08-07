7017deaa67bdf60d0fb33927fafb3974016b0c1a
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2019-08-07 19:25:49 +0200

    Add files via upload

--- apiserver/app.py
+++ apiserver/app.py
@@ -6,6 +6,7 @@ from apiserver.blueprints.simulations import simulations
 from apiserver.blueprints.situations import situations
 from apiserver.blueprints.osint import osint
 from apiserver.blueprints.game import game
+from flask_cors import CORS
 
 
 def create_app():
@@ -18,6 +19,7 @@ def create_app():
     app.register_blueprint(situations)
     app.register_blueprint(osint)
     app.register_blueprint(game)
-
+    CORS(app)
+    
     return app
 
