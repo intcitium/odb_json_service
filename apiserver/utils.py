79a03332df973ecac34c751c3b872322f032b017
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2019-09-26 19:19:23 +0200

    Update utils.py

--- apiserver/utils.py
+++ apiserver/utils.py
@@ -216,7 +216,7 @@ def get_request_payload(request):
         try:
             r = json.loads(request.data)
         except Exception as e:
-            click.echo(e)
+            r = request.args.to_dict(flat=True)
             click.echo(request.data)
     if len(r.keys()) == 1:
         click.echo("Attempting misformed JSON\n%s" % str(r))
