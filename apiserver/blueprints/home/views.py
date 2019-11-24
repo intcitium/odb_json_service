eade1f4e466175eaced75b88b79131f73f862986
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2019-11-24 15:02:42 +0100

    Add files via upload

--- apiserver/blueprints/home/views.py
+++ apiserver/blueprints/home/views.py
@@ -1,35 +1,93 @@
-import json
-from flask import jsonify, Blueprint, send_file, request
-from apiserver.blueprints.home.models import ODB
-from apiserver.utils import get_request_payload, allowed_file, check_for_file
-from werkzeug.utils import secure_filename
-# Application Route specific object instantiation
-home = Blueprint('home', __name__)
-odbserver = ODB()
-odbserver.open_db()
+from flask import jsonify, Blueprint, request
+from apiserver.blueprints.osint.models import OSINT
+from apiserver.utils import get_request_payload
+from flask_cors import CORS
+
+osint = Blueprint('osint', __name__)
+osintserver = OSINT()
+osintserver.open_db()
+osintserver.check_base_book()
+osintserver.fill_db()
+try:
+    osintserver.create_indexes()
+except Exception as e:
+    if "attackpattern.search_fulltext already exists" in str(e):
+        pass
+    else:
+        print(str(e))
+CORS(osint)
 
 
-@home.route('/', methods=['GET'])
+@osint.route('/osint', methods=['GET'])
 def index():
 
     return jsonify({
         "status": 200,
         "message": "Welcome. Basic stats of the endpoint database",
-        "data": odbserver.get_db_stats()
+        "data": {'channels': ['ACLED']}
     })
 
 
-@home.route('/snapshot', methods=['GET'])
-def get_snapshot():
+@osint.route('/osint/acled', methods=['GET'])
+def acled():
 
     return jsonify({
         "status": 200,
-        "message": "Sample data from the file system",
-        "data": odbserver.get_data()
+        "message": "Results from Armed Conflict Location Event Database (ACLED)",
+        "data": osintserver.get_acled()
     })
 
 
-@home.route('/merge_nodes', methods=['POST'])
+@osint.route('/osint/ucdp', methods=['GET'])
+def ucdp():
+
+    return jsonify({
+        "status": 200,
+        "message": "Results from Uppsala Conflict Data Program",
+        "data": osintserver.get_ucdp()
+    })
+
+
+@osint.route('/osint/twitter', methods=['GET'])
+def twitter():
+
+    results, message = osintserver.get_twitter(**(get_request_payload(request)))
+    return jsonify({
+        "status": 200,
+        "message": message,
+        "data": results
+    })
+
+
+@osint.route('/osint/geo_spatial_view', methods=['GET'])
+def geo_spatial_view():
+
+    results, message = osintserver.geo_spatial_view(**(get_request_payload(request)))
+    return jsonify({
+        "status": 200,
+        "message": message,
+        "data": results
+    })
+
+@osint.route('/osint/save', methods=['POST'])
+def save():
+    payload = get_request_payload(request)
+    if payload:
+        case, message = osintserver.save_osint(**payload)
+        return jsonify({
+            "status": 200,
+            "message": message,
+            "data": case
+        })
+    else:
+        return jsonify({
+            "status": 503,
+            "message": "Error saving structure of graph received.",
+            "data": None
+        })
+
+
+@osint.route('/osint/merge_nodes', methods=['POST'])
 def merge_nodes():
     '''
     Base route for merging nodes
@@ -39,59 +97,56 @@ def merge_nodes():
     return jsonify({
         "status": 200,
         "message": "Nodes merged",
-        "data": odbserver.merge_nodes(request)
+        "data": osintserver.merge_osint(**r)
     })
 
-
-@home.route('/return-files/', methods=['POST'])
-def return_files_tut():
+@osint.route('/osint/get_suggestion_items', methods=['POST'])
+def get_suggestion_items():
     '''
-    TODO make this for exporting graphs into CSV
+    Base route for merging nodes
     :return:
     '''
-    return send_file('/var/www/PythonProgramming/PythonProgramming/static/images/python.jpg', attachment_filename='python.jpg')
-
-
-@home.route('/graph_etl_model', methods=['POST'])
-def graph_etl_model():
-    """
-    If only a file is received and a mapping is not required the request is focused on the file
-    If there is only a filename, the service expects that filename to be in the directory and that a model
-    will be used on that file for changing into a graph
-    :return:
-    """
     r = get_request_payload(request)
-    file = check_for_file(request, odbserver)
-    if not file["data"]:
-        if "file" in request.form.to_dict().keys():
-            graph = odbserver.graph_etl_model(
-                json.loads(request.form.to_dict()['model']),
-                odbserver.file_to_frame(request.form.to_dict()["file"])["data"])
-        elif "file" in r.keys() and "model" in r.keys():
-            graph = odbserver.graph_etl_model(r["model"], r["file"])
-        else:
-            return jsonify(file)
-    else:
-        graph = odbserver.graph_etl_model(
-            json.loads(request.form.to_dict()['model']),
-            file["data"])
     return jsonify({
         "status": 200,
-        "data": graph,
-        "filename": file["data"]
+        "message": "Search conducted",
+        "data": osintserver.get_suggestion_items(**r)
     })
 
 
-@home.route('/file_to_graph', methods=['POST'])
-def file_to_graph():
+@osint.route('/osint/cve', methods=['GET'])
+def cve():
+    '''
+    Base route for merging nodes
+    :return:
+    '''
+    return jsonify({
+        "status": 200,
+        "message": "Database updated with latest CVE",
+        "data": osintserver.cve()
+    })
 
-    file = check_for_file(request, odbserver)
-    if not file["data"]:
-        return jsonify(file)
-    else:
-        graph = odbserver.file_to_graph(file["data"])
-        return jsonify({
-            "status": 200,
-            "data": graph,
-            "filename": file["data"]
-        })
+
+@osint.route('/osint/poisonivy', methods=['GET'])
+def poisonivy():
+    '''
+    Base route for merging nodes
+    :return:
+    '''
+    return jsonify({
+        "status": 200,
+        "message": "Database updated with latest CVE",
+        "data": osintserver.poisonivy()
+    })
+
+@osint.route('/osint/get_latest_cti', methods=['GET'])
+def get_latest_cti():
+    '''
+    Base route for merging nodes
+    :return:
+    '''
+    return jsonify({
+        "status": 200,
+        "message": "Database updated with latest CVE",
+        "data": osintserver.run_otx()
+    })
