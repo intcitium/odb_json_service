44471de531faf8310f2bd88ae1833137fc383679
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2019-12-29 14:45:36 +0100

    Add files via upload

--- apiserver/blueprints/chatbot/views.py
+++ apiserver/blueprints/chatbot/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from apiserver.utils import get_datetime, randomString
 from apiserver.blueprints.users.views import odbserver # For authorizing users
 from apiserver.blueprints.chatbot.models import ChatServer
+import click
 
 chatbot = Blueprint('chatbot', __name__)
 # Set up the Chatserver basing the DB storage on the User ODB
@@ -10,10 +11,14 @@ try:
     CB_id = CB.uuid
 except:
     CB_id = "None"
-odbserver.create_user({
-    'userName': CB_id,
-    'email': 'Chatbot@email.com',
-    'passWord': randomString(36)})
+
+try:
+    odbserver.create_user({
+        'userName': CB_id,
+        'email': 'Chatbot@email.com',
+        'passWord': randomString(36)})
+except:
+    click.echo("[%s_Chatbot_init] Setup required" % get_datetime())
 
 
 @chatbot.route('/chatbot/send_message', methods=['POST'])
